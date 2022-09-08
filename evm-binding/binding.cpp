0725c728455f4ed6ef48e595f5e60b2660a51832
samlior <samlior@foxmail.com>
2022-09-08 17:10:00 +0800

    chore: add genesis function for binding

--- evm-binding/binding.cpp
+++ evm-binding/binding.cpp
@@ -21,6 +21,8 @@ using namespace dev::eth;
 
 using Buffer = Napi::Buffer<unsigned char>;
 
+OverlayDB g_mockDB = OverlayDB(DBFactory::create(DatabaseKind::MemoryDB));
+
 class MockLastBlockHashesFace : public LastBlockHashesFace
 {
   public:
@@ -42,6 +44,76 @@ Napi::Value init(const Napi::CallbackInfo &info)
     NoProof::init();
     NoReward::init();
     return info.Env().Undefined();
+}
+
+/**
+ * Init genesis state to database
+ * @param addresses - Address array
+ * @param balances - Balance array
+ * @return Genesis state root(if succeed)
+ */
+Napi::Value genesis(const Napi::CallbackInfo &info)
+{
+    Napi::Env env = info.Env();
+
+    if (info.Length() != 2)
+    {
+        Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+        return env.Undefined();
+    }
+
+    if (!info[0].IsArray() || !info[1].IsArray())
+    {
+        Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+        return env.Undefined();
+    }
+
+    auto addresses = info[0].As<Napi::Array>();
+    auto balances = info[1].As<Napi::Array>();
+
+    if (addresses.Length() != balances.Length())
+    {
+        Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+        return env.Undefined();
+    }
+
+    std::vector<std::pair<Address, u256>> genesisInfo;
+    for (std::size_t i = 0; i < addresses.Length(); i++)
+    {
+        auto index = Napi::Number::New(env, i);
+        auto address = addresses.Get(index);
+        auto balance = balances.Get(index);
+        if (!address.IsString() || !balance.IsString())
+        {
+            Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+            return env.Undefined();
+        }
+        genesisInfo.push_back({Address(address.As<Napi::String>()), u256(fromHex(balance.As<Napi::String>()))});
+    }
+
+    State state(0, g_mockDB, BaseState::Empty);
+    auto sp = state.savepoint();
+    try
+    {
+        for (const auto &pair : genesisInfo)
+        {
+            state.addBalance(pair.first, pair.second);
+        }
+        state.commit(State::CommitBehaviour::KeepEmptyAccounts);
+        return Napi::String::From(env, toHex(state.rootHash()));
+    }
+    catch (const std::exception &err)
+    {
+        state.rollback(sp);
+        Napi::Error::New(env, err.what()).ThrowAsJavaScriptException();
+        return env.Undefined();
+    }
+    catch (...)
+    {
+        state.rollback(sp);
+        Napi::Error::New(env, "Unknown error").ThrowAsJavaScriptException();
+        return env.Undefined();
+    }
 }
 
 /**
@@ -88,10 +160,9 @@ Napi::Value runTx(const Napi::CallbackInfo &info)
         // create env info object
         EnvInfo envInfo(header, lbh, gasUsed, params.chainID);
         // TODO: get leveldb instance
-        OverlayDB db(DBFactory::create(DatabaseKind::MemoryDB));
-        // create state manager object
         // TODO: BaseState
-        State state(0, db, BaseState::PreExisting);
+        // create state manager object
+        State state(0, g_mockDB, BaseState::PreExisting);
         state.setRoot(stateRoot);
         // execute transaction
         auto [result, receipt] = state.execute(envInfo, *engine, tx, Permanence::Committed);
@@ -115,6 +186,7 @@ Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
     exports.Set(Napi::String::New(env, "init"), Napi::Function::New(env, init));
     exports.Set(Napi::String::New(env, "runTx"), Napi::Function::New(env, runTx));
+    exports.Set(Napi::String::New(env, "genesis"), Napi::Function::New(env, genesis));
     return exports;
 }
 
