7e44705588467303f0efb588a885bfd08102b9dd
samlior <samlior@foxmail.com>
2022-10-21 11:02:00 +0800

    chore: improve loading binding logic

--- src/binding.js
+++ src/binding.js
@@ -1,19 +1,22 @@
 const bindings = require('bindings')
 
-const tryList = [
+const releaseTryList = [
     // node-waf and gyp_addon (a.k.a node-gyp)
-    ['module_root', 'build', 'Debug', 'bindings'],
     ['module_root', 'build', 'Release', 'bindings'],
     // Production "Release" buildtype binary (meh...)
     ['module_root', 'compiled', 'platform', 'arch', 'bindings'],
 ]
 
+const debugTryList = [
+    ['module_root', 'build', 'Debug', 'bindings']
+]
+
 module.exports = function (name) {
     try {
         // load release binding
-        return bindings({ bindings: name, try: tryList })
+        return bindings({ bindings: name, try: releaseTryList })
     } catch {
         // load debug binding
-        return bindings({ bindings: name + 'd', try: tryList })
+        return bindings({ bindings: name + 'd', try: debugTryList })
     }
 }
